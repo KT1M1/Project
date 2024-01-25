c3762b24c05d7a054ec01b3c5d99c28ad1f449e7
KT1M1 <ktimea.205@gmail.com>
2024-01-25 11:15:40 +0100

    Small Changes In Login Style

--- client/pages/login.php
+++ client/pages/login.php
@@ -12,7 +12,7 @@ $login_checked = "checked";
 $signin_checked = "";
 
 
-if( isset( $_POST['signin'] ) && $_POST['signin'] == '1' ) {
+if(isset( $_POST['signin'] ) && $_POST['signin'] == '1') {
 
     $message = login_user(
         $_POST['user_name'],
@@ -39,7 +39,7 @@ if( isset( $_POST['signin'] ) && $_POST['signin'] == '1' ) {
 /**
  * If the user already logged in redirect to main page.
  */
-if ( session_status() !== PHP_SESSION_ACTIVE ) {
+if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
 
@@ -53,79 +53,89 @@ if (isset($_SESSION['user'])) {
 
 <!DOCTYPE html>
 <html lang="hu">
+
 <head>
-  <link rel="stylesheet" href="/client/assets/css/login.css"/>
-  <title>Form</title>
-  <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/client/assets/css/login.css" />
+    <title>Form</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
-  <div class="login-wrap">
-    <div class="login-html">
-      <input id="tab-1" type="radio" name="tab" class="sign-in" <?php echo $login_checked; ?>><label for="tab-1" class="tab">Bejelentkezés</label>
-      <input id="tab-2" type="radio" name="tab" class="sign-up"  <?php echo $signin_checked; ?>><label for="tab-2" class="tab">Regisztráció</label>
-      <div class="login-form">
-        <form action="" method="post" class="sign-in-htm">
-          <div class="group">
-            <label for="user" class="label">Felhasználónév</label>
-            <input id="user" name="user_name" type="text" class="input">
-          </div>
-          <div class="group">
-            <label for="pass" class="label">Jelszó</label>
-            <input id="pass" name="password" type="password" class="input" data-type="password">
-          </div>
-          <div class="group">
-            <input id="check" name="stay_signed" type="checkbox" class="check" checked>
-            <label for="check"><span class="icon"></span> Maradjak bejelentkezve</label>
-          </div>
-          <div class="group">
-            <input type="submit" class="button" value="Bejelentkezés">
-          </div>
-          <div class="hr"></div>
-          <div class="foot-lnk">
-            <a href="#forgot">Elfelejtette jelszavát?</a>
-          </div>
-          <input type="hidden" name="signin" value="1">
-        </form>
-        <form action="" method="post" class="sign-up-htm">
-          <div class="group">
-            <label for="fullname" class="label">Teljes név</label>
-            <input id="fullname" name="fullname" type="text" class="input" value="<?php echo $fullname; ?>">
-          </div>
-          <div class="group">
-            <label for="user" class="label">Felhasználónév</label>
-            <input id="user" name="user_name" type="text" class="input" value="<?php echo $user_name; ?>">
-          </div>
-          <div class="group">
-            <label for="password" class="label">Jelszó</label>
-            <input id="password" name="password" type="password" class="input" data-type="password">
-          </div>
-          <div class="group">
-            <label for="password_again" class="label">Jelszó ismét</label>
-            <input id="password_again" name="password_again" type="password" class="input" data-type="password">
-          </div>
-          <div class="group">
-            <label for="email" class="label">Email cím</label>
-            <input id="email" name="email" type="email" class="input" value="<?php echo $email; ?>">
-          </div>
-          <div class="group">
-            <input type="submit" class="button" value="Regisztráció">
-          </div>
-          <div class="hr"></div>
-          <div class="foot-lnk">
-            <label for="tab-1">Már regisztrált?</a>
-          </div>
-          <input type="hidden" name="signup" value="1">
-        </form>
-        <div style="
-            position: absolute;
-            right: 0;
-        ">
-            <?php
-                echo $message;
-            ?>
+
+    <div class="login-wrap">
+        <div class="login-html">
+            <h1 class="page-name">RecipeShare</h1>
+            <input id="tab-1" type="radio" name="tab" class="sign-in" <?php echo $login_checked; ?>><label for="tab-1"
+                class="tab">Bejelentkezés</label>
+            <input id="tab-2" type="radio" name="tab" class="sign-up" <?php echo $signin_checked; ?>><label for="tab-2"
+                class="tab">Regisztráció</label>
+                <div class="login-form">
+            <form action="" method="post" class="sign-in-htm">
+                <div class="group">
+                    <label for="user" class="label">Felhasználónév</label>
+                    <input id="user" name="user_name" type="text" class="input">
+                </div>
+                <div class="group">
+                    <label for="pass" class="label">Jelszó</label>
+                    <input id="pass" name="password" type="password" class="input" data-type="password">
+                </div>
+                <div class="group">
+                    <input id="check" name="stay_signed" type="checkbox" class="check" checked>
+                    <label for="check"><span class="icon"></span> Maradjak bejelentkezve</label>
+                </div>
+                <div class="group">
+                    <input type="submit" class="button" value="Bejelentkezés">
+                </div>
+                <div class="hr"></div>
+                <div class="foot-lnk">
+                    <a href="#forgot">Elfelejtette jelszavát?</a>
+                </div>
+                <input type="hidden" name="signin" value="1">
+            </form>
+
+            <form action="" method="post" class="sign-up-htm">
+                <div class="group">
+                    <label for="fullname" class="label">Teljes név</label>
+                    <input id="fullname" name="fullname" type="text" class="input" value="<?php echo $fullname; ?>">
+                </div>
+                <div class="group">
+                    <label for="user" class="label">Felhasználónév</label>
+                    <input id="user" name="user_name" type="text" class="input" value="<?php echo $user_name; ?>">
+                </div>
+                <div class="group">
+                    <label for="password" class="label">Jelszó</label>
+                    <input id="password" name="password" type="password" class="input" data-type="password">
+                </div>
+                <div class="group">
+                    <label for="password_again" class="label">Jelszó ismét</label>
+                    <input id="password_again" name="password_again" type="password" class="input" data-type="password">
+                </div>
+                <div class="group">
+                    <label for="email" class="label">Email cím</label>
+                    <input id="email" name="email" type="email" class="input" value="<?php echo $email; ?>">
+                </div>
+                <div class="group">
+                    <input type="submit" class="button" value="Regisztráció">
+                </div>
+                <div class="hr"></div>
+                <div class="foot-lnk">
+                    <label for="tab-1">Már regisztrált?</a>
+                </div>
+                <input type="hidden" name="signup" value="1">
+            </form>
+            <div style="
+          position: absolute;
+          right: 0;
+      ">
+                <?php
+              echo $message;
+          ?>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </body>
+
 </html>
