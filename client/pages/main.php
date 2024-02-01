09ce3f0670ebaf04b2f2be7564e36e267699052a
KT1M1 <ktimea.205@gmail.com>
2024-02-01 18:34:09 +0100

    Format Document for Improved Readability

--- client/pages/main.php
+++ client/pages/main.php
@@ -12,64 +12,66 @@
             <h2 class="divider myanchor" id="popular">LEGKEDVELTEBB RECEPTEK</h2>
             <hr>
         </div>
-    
+
         <div class="introduct-text">
-            <p>Itt a közösség ízlése és preferenciái határozzák meg, hogy melyik étel marad a dobogón. <br> Ez egy dinamikus és folyamatosan változó értékelési rendszer, amely nem korlátozódik az ételek összetevőire vagy elkészítésére.</p>
+            <p>Itt a közösség ízlése és preferenciái határozzák meg, hogy melyik étel marad a dobogón. <br> Ez egy
+                dinamikus és folyamatosan változó értékelési rendszer, amely nem korlátozódik az ételek összetevőire
+                vagy elkészítésére.</p>
         </div>
     </div>
 
     <div class="row popular">
-    <!--Rank 1-->
+        <!--Rank 1-->
         <div class="col-12 col-sm-6 col-lg-4">
-            <a href="page/show_recipe/7">
+            <a href="page/show_recipe/13">
                 <div class="card-div">
                     <div class="rank-cube">#1</div>
                     <div class="card-top">
-                        <img class="card-pic" src="./client/assets/img/bolognese.jpg" alt="">
+                        <img class="card-pic" src="./client/assets/img/bolognese.jpg">
                     </div>
                     <div class="card-bottom">
                         <div class="card-text">
                             <p>Bolognai Spagetti Egyszerűen</p>
                         </div>
-                        <div  class="card-rate">
-                            <img class="img-fluid card-heart" src="./client/assets/img/heart.png" alt="" srcset="">
+                        <div class="card-rate">
+                            <img class="img-fluid card-heart" src="./client/assets/img/heart.png">
                             <p class="card-heart">20</p>
                         </div>
                     </div>
                 </div>
             </a>
         </div>
-    <!--Rank 2-->
+        <!--Rank 2-->
         <div class="col-12 col-sm-6 col-lg-4">
             <div class="card-div">
                 <div class="rank-cube">#2</div>
                 <div class="card-top">
-                    <img class="card-pic" src="./client/assets/img/bolognese.jpg" alt="">
+                    <img class="card-pic" src="./client/assets/img/bolognese.jpg">
                 </div>
                 <div class="card-bottom">
                     <div class="card-text">
                         <p>Gyors Bolognai Spagetti</p>
                     </div>
-                    <div  class="card-rate">
-                        <img class="img-fluid card-heart" src="./client/assets/img/heart.png" alt="" srcset="">
+                    <div class="card-rate">
+                        <img class="img-fluid card-heart" src="./client/assets/img/heart.png">
                         <p class="card-heart">18</p>
                     </div>
                 </div>
             </div>
         </div>
-    <!--Rank 3-->
+        <!--Rank 3-->
         <div class="col-12 col-sm-6 col-lg-4">
             <div class="card-div">
                 <div class="rank-cube">#3</div>
                 <div class="card-top">
-                    <img class="card-pic" src="./client/assets/img/bolognese.jpg" alt="">
+                    <img class="card-pic" src="./client/assets/img/bolognese.jpg">
                 </div>
                 <div class="card-bottom">
                     <div class="card-text">
                         <p>Könnyű Bolognai Spagetti</p>
                     </div>
-                    <div  class="card-rate">
-                        <img class="img-fluid card-heart" src="./client/assets/img/heart.png" alt="" srcset="">
+                    <div class="card-rate">
+                        <img class="img-fluid card-heart" src="./client/assets/img/heart.png">
                         <p class="card-heart">13</p>
                     </div>
                 </div>
@@ -84,25 +86,25 @@
     <div class="ing-icon-container">
         <div class="ing-nav-piece">
             <a href="" class="rotate">
-                <img class="img-fluid ing-icon rotate-image" src="./client/assets/img/mealTypes/meat2.jpg" alt="" srcset="">
+                <img class="img-fluid ing-icon rotate-image" src="./client/assets/img/mealTypes/meat2.jpg">
                 <p class="ing-piece-txt">Hús</p>
             </a>
         </div>
         <div class="ing-icon-piece">
             <a href="" class="rotate">
-                <img class="img-fluid ing-icon rotate-image" src="./client/assets/img/mealTypes/fish.jpg" alt="" srcset="">
+                <img class="img-fluid ing-icon rotate-image" src="./client/assets/img/mealTypes/fish.jpg">
                 <p class="ing-piece-txt">Hal</p>
             </a>
         </div>
         <div class="ing-icon-piece">
             <a href="" class="rotate">
-                <img class="img-fluid ing-icon rotate-image" src="./client/assets/img/mealTypes/pasta1.jpg" alt="" srcset="">
+                <img class="img-fluid ing-icon rotate-image" src="./client/assets/img/mealTypes/pasta1.jpg">
                 <p class="ing-piece-txt">Tészta</p>
             </a>
         </div>
         <div class="ing-icon-piece">
             <a href="" class="rotate">
-                <img class="img-fluid ing-icon rotate-image" src="./client/assets/img/mealTypes/vegs1.jpg" alt="" srcset="">
+                <img class="img-fluid ing-icon rotate-image" src="./client/assets/img/mealTypes/vegs1.jpg">
                 <p class="ing-piece-txt">Zöldség</p>
             </a>
         </div>
@@ -121,7 +123,12 @@
     <div class="row">
         <div class="col-12 col-md-6">
             <div class="course-container">
-                <p class="course-text"> &emsp; A főétel jellemzően a leglaktatóbb fogás, amely az <span class="highlight-text"> ebédet  vagy a vacsorát </span> alkotja, és az étkezés középpontjában áll. <br> &emsp; Ezen ételkategória rendkívül változatos, és számos kulturális, regionális és személyes ízlési preferenciát tükröző variációval rendelkezik. <br> &emsp; Leggyakrabban <span class="highlight-text"> húsok, halak</span> vagy más fehérjeforrások alkotják a főételt, de vegetáriánus vagy vegán ételek esetében is számos izgalmas lehetőség áll rendelkezésre.</p>
+                <p class="course-text"> &emsp; A főétel jellemzően a leglaktatóbb fogás, amely az <span
+                        class="highlight-text"> ebédet vagy a vacsorát </span> alkotja, és az étkezés középpontjában
+                    áll. <br> &emsp; Ezen ételkategória rendkívül változatos, és számos kulturális, regionális és
+                    személyes ízlési preferenciát tükröző variációval rendelkezik. <br> &emsp; Leggyakrabban <span
+                        class="highlight-text"> húsok, halak</span> vagy más fehérjeforrások alkotják a főételt, de
+                    vegetáriánus vagy vegán ételek esetében is számos izgalmas lehetőség áll rendelkezésre.</p>
             </div>
             <div class="category-btn">
                 <button type="button" class="btn ctg-button">Ismerd meg a kategória további fogásait!</button>
@@ -131,7 +138,7 @@
         <div class="col-12 col-md-6">
             <div class="card-div">
                 <div class="card-top">
-                    <img class="card-pic" src="./client/assets/img/bologna.jpg" alt="">
+                    <img class="card-pic" src="./client/assets/img/bologna.jpg">
                 </div>
                 <div class="card-bottom">
                     <div class="card-text">
@@ -153,7 +160,11 @@
     <div class="row snack">
         <div class="col-12 col-md-6">
             <div class="course-container">
-                <p class="course-text"> &emsp; A köztes étkezések kategóriájába tartozó receptek rendkívül változatosak és izgalmasak lehetnek, hiszen kielégítik az éhségünket a főétkezések között, miközben finom és tápláló ételeket kínálnak. <br> &emsp; Ezek a könnyű és ízletes fogások ideálisak a nap bármely szakaszában, legyen szó <span  class="highlight-text"> reggeliről,</span> délutáni <span  class="highlight-text"> nasiról</span> vagy esti snackről.</p>
+                <p class="course-text"> &emsp; A köztes étkezések kategóriájába tartozó receptek rendkívül változatosak
+                    és izgalmasak lehetnek, hiszen kielégítik az éhségünket a főétkezések között, miközben finom és
+                    tápláló ételeket kínálnak. <br> &emsp; Ezek a könnyű és ízletes fogások ideálisak a nap bármely
+                    szakaszában, legyen szó <span class="highlight-text"> reggeliről,</span> délutáni <span
+                        class="highlight-text"> nasiról</span> vagy esti snackről.</p>
             </div>
             <div class="category-btn">
                 <button type="button" class="btn ctg-button">Ismerd meg a kategória további fogásait!</button>
@@ -163,7 +174,7 @@
         <div class="col-12 col-md-6">
             <div class="card-div">
                 <div class="card-top">
-                    <img class="card-pic" src="./client/assets/img/caprese.jpg" alt="">
+                    <img class="card-pic" src="./client/assets/img/caprese.jpg">
                 </div>
                 <div class="card-bottom">
                     <div class="card-text">
@@ -185,7 +196,13 @@
     <div class="row">
         <div class="col-12 col-md-6">
             <div class="course-container">
-                <p class="course-text"> &emsp; A desszertek kategóriájába tartozó receptek számtalan lehetőséget kínálnak az <span class="highlight-text">édes</span> élvezetek világában. <br> &emsp; Ezek az ételek nem csupán egyszerűen kielégítik az édesszájúak vágyait, hanem igazi művészi alkotások is lehetnek. <br> &emsp; A különféle <span class="highlight-text">sütemények, torták, fagylaltok</span> és egyéb édes finomságok minden alkalomra megtalálhatók, és segítenek megkoronázni az étkezést egy kellemes és édes lezárással.</p>
+                <p class="course-text"> &emsp; A desszertek kategóriájába tartozó receptek számtalan lehetőséget
+                    kínálnak az <span class="highlight-text">édes</span> élvezetek világában. <br> &emsp; Ezek az ételek
+                    nem csupán egyszerűen kielégítik az édesszájúak vágyait, hanem igazi művészi alkotások is lehetnek.
+                    <br> &emsp; A különféle <span class="highlight-text">sütemények, torták, fagylaltok</span> és egyéb
+                    édes finomságok minden alkalomra megtalálhatók, és segítenek megkoronázni az étkezést egy kellemes
+                    és édes lezárással.
+                </p>
             </div>
             <div class="category-btn">
                 <button type="button" class="btn ctg-button">Ismerd meg a kategória további fogásait!</button>
@@ -195,7 +212,7 @@
         <div class="col-12 col-md-6">
             <div class="card-div">
                 <div class="card-top">
-                    <img class="card-pic" src="./client/assets/img/csokisbrownie.jpg" alt="">
+                    <img class="card-pic" src="./client/assets/img/csokisbrownie.jpg">
                 </div>
                 <div class="card-bottom">
                     <div class="card-text">
@@ -203,6 +220,7 @@
                     </div>
                 </div>
             </div>
+            </a>
         </div>
     </div>
 </div>
